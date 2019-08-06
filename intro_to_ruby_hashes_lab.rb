266f3f771ad9ac8221358b18046dac1555ed0894
James Mobus <mobusj@live.com>
2019-08-06 21:38:16 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -22,8 +22,7 @@ end
 
 def my_hash_creator(key, value)
   hash = { key value }
-  # return a hash that includes the key and value parameters passed into this method
-end
+  end
 
 def read_from_hash(hash, key)
   # return the correct value using the hash and key parameters
